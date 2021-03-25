124801bd306a71148c5fa20f13dc6abb2c7dce36
Katie Dsouza <ksdsouza@uwaterloo.ca>
2021-03-25 10:47:20 -0400

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2743ea4e7a0b6b9cd3a2";
+/******/ 	var hotCurrentHash = "bd8e03ecee687078052c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
