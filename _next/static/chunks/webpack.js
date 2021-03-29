c800bddbb49fcedc5ba6bbd32f4306ce52a58885
Katie Dsouza <katiedsouza.13@gmail.com>
2021-03-29 08:56:05 -0400

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "78bbb5e8f644bf2c7fcf";
+/******/ 	var hotCurrentHash = "bdd4d18d80489e0900f4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
