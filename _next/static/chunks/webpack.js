25da4296cb87b26a2045f4316416fa2a08983376
ddlan <L.danny18725@gmail.com>
2021-12-26 16:41:18 -0500

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "27e31207f020ee7e19be";
+/******/ 	var hotCurrentHash = "231173ccab03c10d7371";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
