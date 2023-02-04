6d432b7bb865d0407fa04bd93297c9928e0c5508
A S M Taseen <taseen951753@gmail.com>
2023-02-04 14:11:44 -0500

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a321e0c0772fa771ad7";
+/******/ 	var hotCurrentHash = "97d88ddeedd2f22eb041";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
