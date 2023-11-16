a51328d4eaacdf2387d867097e16503f1befe371
Abhinav Gupta <abhinavgupta1882002@gmail.com>
2023-11-15 21:50:12 -0500

    Updates

---
+++ _next/static/webpack/styles.3c7b6efc0e2f40405761.hot-update.js
@@ -0,0 +1,45 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./components/common/RectangleDivider.scss":
+false,
+
+/***/ "./components/common/ValuesContent.scss":
+false,
+
+/***/ "./components/layout/LandingPage.scss":
+false,
+
+/***/ "./components/layout/panels/EventsPanel.scss":
+false,
+
+/***/ "./components/layout/panels/PartnerPanel.scss":
+false,
+
+/***/ "./components/layout/panels/ProjectsPanel.scss":
+false,
+
+/***/ "./components/layout/panels/ValuesPanel.scss":
+false,
+
+/***/ 10:
+false,
+
+/***/ 4:
+false,
+
+/***/ 5:
+false,
+
+/***/ 6:
+false,
+
+/***/ 7:
+false,
+
+/***/ 8:
+false,
+
+/***/ 9:
+false
+
+})
